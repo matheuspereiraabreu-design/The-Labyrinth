bb29bd4a16ca2205226ba667410ae30e319f9f94
matheuspereiraabreu-design <matheus.pereira.abreu@escola.pr.gov.br>
2026-09-06 00:07:45 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function startGame() {
+    console.log("O jogo começou!");
+
+    window.location.href = "historia.html";
+}
+
+function personagens() {
+    window.location.href = "personagens.html";
+}
